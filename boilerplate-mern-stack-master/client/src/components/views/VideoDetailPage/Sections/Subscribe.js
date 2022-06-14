3f25ac2a6ceb421b235ce2e61e1d43b6106dfcc2
daseul home <sisial1002@gmail.com>
2022-06-14 23:32:13 +0900

    subscribe 프론트까지

---
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react'
+//import Axios from 'axios';
+import { Button } from 'antd';
+
+function Subscribe() {
+    return (
+        <div>
+            <Button type="primary">
+                0 Subscribe
+            </Button>
+        </div>
+
+    )
+
+}
+
+export default Subscribe
