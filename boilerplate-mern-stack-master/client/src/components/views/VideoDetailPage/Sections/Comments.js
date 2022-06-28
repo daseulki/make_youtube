bf68329119b4d61abafe2e931fca6cefa723c42b
Daseulki <sisial1002@gmail.com>
2022-06-28 18:20:40 +0900

    comment 페이지 생성

---
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/Sections/Comments.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react'
+// import Axios from 'axios';
+import { Button } from 'antd';
+
+function Comment() {
+
+  
+
+    return (
+        <div>
+           Comment
+        </div>
+
+    )
+
+}
+
+export default Comment
