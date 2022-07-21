4224772684c3e4020ec0d238d92b198e1fc82c69
Daseulki <sisial1002@gmail.com>
2022-07-21 17:26:11 +0900

    디버깅완료

--- boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -4,7 +4,7 @@ import Axios from 'axios'
 import SideVideo from './Sections/SideVideo'
 import Subscribe from './Sections/Subscribe'
 import Comments from './Sections/Comments'
-
+import LikeDislikes from './Sections/LikeDislikes';
 
 function VideoDetailPage(props) {
     const videoId = props.match.params.videoId
@@ -48,7 +48,11 @@ function VideoDetailPage(props) {
                     <div style={{width: '100%', padding:'3rem 4rem'}}>
                         <video style={{width: '100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
 
-                        <List.Item actions={[<Subscribe userTo={VideoDetail.writer._id}/>]}>
+                        <List.Item actions={[
+                            <LikeDislikes video videoId={videoId} userId={localStorage.getItem('userId')}  />,
+                            <Subscribe userTo={VideoDetail.writer._id}/>
+                        ]}>
+                            
                             <List.Item.Meta 
                                 avatar={<Avatar src={VideoDetail.writer.image}/>}
                                 title={VideoDetail.writer.name}
